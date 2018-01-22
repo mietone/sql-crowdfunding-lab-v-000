1d8fcb5882e361abffbe37f108e058ceda4eb0ba
Miwa <paintfx@gmail.com>
2018-01-22 17:37:50 +0000

    automatically backed up by learn

--- lib/create.sql
+++ lib/create.sql
@@ -3,8 +3,8 @@ CREATE TABLE projects (
   title TEXT,
   category TEXT,
   funding_goal INTEGER,
-  start_date DATE,
-  end_date DATE
+  start_date TEXT,
+  end_date TEXT
 );
 
 CREATE TABLE users (
