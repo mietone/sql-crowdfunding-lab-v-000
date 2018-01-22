7a69529654b36797917c4b5ed760d6ec1ea5bdd8
Miwa <paintfx@gmail.com>
2018-01-22 17:42:13 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,4 +1,23 @@
 INSERT INTO projects VALUES ("project_1", "cat1", 1000000, 2018-01-01, 2018-03-28);
+INSERT INTO projects VALUES ("project_2", "cat1", 100000, 2018-04-01, 2018-04-01);
+INSERT INTO projects VALUES ("project_3", "cat1", 1050000, 2018-01-01, 2018-02-28);
+INSERT INTO projects VALUES ("project_4", "cat1", 200000, 2018-01-01, 2018-03-28);
+INSERT INTO projects VALUES ("project_5", "cat1", 900000, 2018-06-01, 2018-05-28);
+INSERT INTO projects VALUES ("project_6", "cat1", 8360000, 2018-01-01, 2018-03-28);
+INSERT INTO projects VALUES ("project_7", "cat1", 1200000, 2018-01-01, 2018-08-28);
+INSERT INTO projects VALUES ("project_8", "cat1", 1500000, 2018-01-01, 2018-06-28);
+INSERT INTO projects VALUES ("project_9", "cat1", 830000, 2018-01-01, 2018-10-28);
+INSERT INTO projects VALUES ("project_10", "cat1", 6200000, 2018-01-01, 2018-11-28);
+INSERT INTO projects VALUES ("project_11", "cat1", 1200000, 2018-03-01, 2018-03-28);
+INSERT INTO projects VALUES ("project_12", "cat1", 1600000, 2018-01-01, 2018-03-28);
+INSERT INTO projects VALUES ("project_13", "cat1", 1800000, 2018-05-01, 2018-03-28);
+INSERT INTO projects VALUES ("project_14", "cat1", 1000000, 2018-01-01, 2018-04-28);
+INSERT INTO projects VALUES ("project_15", "cat1", 1500000, 2018-01-01, 2018-06-28);
+INSERT INTO projects VALUES ("project_16", "cat1", 1900000, 2018-07-01, 2018-11-28);
+INSERT INTO projects VALUES ("project_17", "cat1", 9400000, 2018-01-01, 2018-03-28);
+INSERT INTO projects VALUES ("project_18", "cat1", 7300000, 2018-09-01, 2018-12-28);
+INSERT INTO projects VALUES ("project_19", "cat1", 8200000, 2018-03-01, 2018-05-28);
+INSERT INTO projects VALUES ("project_20", "cat1", 500000, 2018-11-01, 2018-12-28);
 
 -- title TEXT,
 -- category TEXT,
