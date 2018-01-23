2292f2da1ba50284c0c8e9341638730105cb0adf
Miwa <paintfx@gmail.com>
2018-01-23 01:08:23 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -27,7 +27,7 @@ def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_
 FROM projects
 JOIN pledges
   ON projects.id = pledges.project_id
-WHERE projects.funding_goal > pledges.amount
+WHERE projects.funding_goal - pledges.amount >= 0
 GROUP BY projects.title;"
 end
 
