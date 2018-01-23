f5b4003162cb837d05e9d5b5cb5ff2d17935bb1e
Miwa <paintfx@gmail.com>
2018-01-23 00:38:29 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -9,9 +9,9 @@
 def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_name
 "SELECT title, SUM(pledges.amount)
 FROM projects
-LEFT JOIN pledges
+JOIN pledges
   ON projects.id = pledges.project_id
-ORDER BY title ASC;"
+GROUP BY projects;"
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
