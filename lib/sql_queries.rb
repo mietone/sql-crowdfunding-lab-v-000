99617be9edfd340064512877d528bba6b8a7c12b
Miwa <paintfx@gmail.com>
2018-01-23 00:44:37 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -15,7 +15,7 @@ GROUP BY projects.title;"
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
-"SELECT name, age
+"SELECT name, age, pledges.amount
 FROM users
 JOIN pledges
   ON users.id = pledges.user_id
