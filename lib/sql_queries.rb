feb83a58164640a5eb1d0924a5c7ee7ed1b364c8
Miwa <paintfx@gmail.com>
2018-01-23 01:49:28 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -32,12 +32,12 @@ HAVING amount_left >= 0;"
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
-"SELECT users.name, pledges.amount
+"SELECT users.name, SUM(pledges.amount)
 FROM users
 JOIN pledges
   ON users.id = pledges.user_id
 GROUP BY users.name
-ORDER BY pledges.amount ASC,
+ORDER BY SUM(pledges.amount) ASC,
   users.name ASC"
 end
 
